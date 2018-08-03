fce2b4dfd201282553f8b2a97a1f998dd28e2ae1
zqh <530068718@qq.com>
2018-08-03 10:00:44 +0800

    Modify RedisModels index.js

--- Models/RedisModels/index.js
+++ Models/RedisModels/index.js
@@ -1,19 +1,9 @@
-function MQTTTopics(redis) {
-  this._ioredis = redis;
-}
-
-MQTTTopics.prototype.addTopics = function (did, config) {
-  const pipeline = this._ioredis.pipeline();
-  const topicsValues = `${config.cloudId}:${config.loraId}`;
-
-  config.suffix.forEach((element) => {
-    let topic = config.prefix + did + element;
-    pipeline.sadd(topic, topicsValues);
-  });
-
-  return pipeline.exec();
-};
+'use strict';
 
 module.exports = {
-  MQTTTopics,
+  DeDuplication: require('./DeDuplication'),
+  DevRXInfoQueue: require('./DevRXInfoQueue'),
+  DownlinkCmdQueue: require('./DownlinkCmdQueue'),
+  MacCmdQueue: require('./MacCmdQueue'),
+  MessageQueue: require('./MessageQueue'),
 };
