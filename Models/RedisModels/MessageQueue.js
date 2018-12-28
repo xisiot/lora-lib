1038c9ac86778db26b605dba6a2061bb3bd0bdc2
GaoJia <1832735568@qq.com>
2018-12-28 17:19:10 +0800

    add downlink API function

--- Models/RedisModels/MessageQueue.js
+++ Models/RedisModels/MessageQueue.js
@@ -69,4 +69,15 @@ MessageQueue.prototype.produce = function (mq, src, protoBufUnit, AppEUI) {
   });
 };
 
+MessageQueue.prototype.produceByHTTP = function (mq, downlinkString) {
+  let _this = this;
+  _this.consume(mq)
+    .then(() => {
+      let writeObj;
+      writeObj.pbdata = downlinkString;
+      writeObj.data = downlinkString;
+      writeObj.aggregation = 0;
+      return _this._ioredis.rpush(mq, JSON.stringify(writeObj))
+    })
+}
 module.exports = MessageQueue;
